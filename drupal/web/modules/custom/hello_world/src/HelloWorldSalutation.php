9fd64e36eaeae256ae9675e0884cee4a5e3c405d
Selhar <selhar@protonmail.com>
2020-03-22 12:49:37 -0300

    feat: added Salutation service

---
+++ drupal/web/modules/custom/hello_world/src/HelloWorldSalutation.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\hello_world;
+
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+
+/**
+ * Prepares the salutation to the world
+ */
+class HelloWorldSalutation {
+  use StringTranslationTrait;
+
+  /**
+   * Returns the salutation
+   */
+  public function getSalutation() {
+    $time = new \DateTime();
+    $time = (int) $time->format('G');
+    if ($time >= 00 && $time < 12) {
+      return $this->t('Good morning world');
+    }
+
+    if ($time >= 12 && $time < 18) {
+      return $this->t('Good afternoon world');
+    }
+
+    if ($time >= 18) {
+      return $this->t('Good evening world');
+    }
+  }
+}
